e49ca29450e30cb9b24a033186f7b3289024c5fb
Ilya Shchepetkov <ilya.shchepetkov@gmail.com>
2022-11-30 09:35:42 +0000

    Support running tests natively on other archs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,61 @@
 import os
 import pytest
+import re
+import subprocess
 
 
 def pytest_collection_modifyitems(config, items):
-    cif_path = os.environ.get('CIF', 'cif')
+    if os.environ.get("CIF"):
+        aspectator_path = os.environ.get("CIF")[:-3] + "aspectator"
+    else:
+        aspectator_path = os.path.join(
+            os.path.dirname(__file__), "..", "inst", "bin", "aspectator"
+        )
+
+    arch = get_arch(aspectator_path)
 
     skip_arm = pytest.mark.skipif(
-        'arm' not in os.path.basename(cif_path),
-        reason='arm cif is not available'
+        "arm" not in arch, reason="arm cif is not available"
     )
 
     skip_not_arm = pytest.mark.skipif(
-        'arm' in os.path.basename(cif_path),
-        reason='arm cif is available'
+        "arm" in arch, reason="arm cif is available"
     )
 
     skip_aarch64 = pytest.mark.skipif(
-        'aarch64' not in os.path.basename(cif_path),
-        reason='aarch64 cif is not available'
+        "aarch64" not in arch, reason="aarch64 cif is not available"
     )
 
     skip_x86_64 = pytest.mark.skipif(
-        'cif' != os.path.basename(cif_path),
-        reason='x86_64 cif is not available'
+        "x86_64" not in arch, reason="x86_64 cif is not available"
     )
 
     for item in items:
-        if 'arm' in item.keywords:
+        if "arm" in item.keywords:
             item.add_marker(skip_arm)
 
-        if 'not_arm' in item.keywords:
+        if "not_arm" in item.keywords:
             item.add_marker(skip_not_arm)
 
-        if 'aarch64' in item.keywords:
+        if "aarch64" in item.keywords:
             item.add_marker(skip_aarch64)
 
-        if 'x86_64' in item.keywords:
+        if "x86_64" in item.keywords:
             item.add_marker(skip_x86_64)
+
+
+def get_arch(gcc_path):
+    r = subprocess.run(
+        f"{gcc_path} -v",
+        shell=True,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+    m = re.search(r"Target: (.*?)-", r.stdout)
+
+    if not m:
+        raise RuntimeError(f"Can't determine CIF arch: {gcc_path}")
+
+    return m.group(1)
